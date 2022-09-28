43bc7f193b4e5992ac0ddb7aaf5ca9bbad4860d0
edulongodevgeo <eduardosl.geo@gmail.com>
2022-09-28 17:34:14 -0300

    Projeton

---
+++ src/main/java/com/edu/gamesandchips/GamesandchipsApplication.java
@@ -0,0 +1,13 @@
+package com.edu.gamesandchips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesandchipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesandchipsApplication.class, args);
+	}
+
+}
