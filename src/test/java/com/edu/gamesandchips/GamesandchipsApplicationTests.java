5d8e2e8c2f43ef0611abdd94c06d4180c11ece4f
Eduardo Longo <65884093+edulongodevgeo@users.noreply.github.com>
2022-09-28 18:10:53 -0300

    Add files via upload

---
+++ src/test/java/com/edu/gamesandchips/GamesandchipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.gamesandchips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesandchipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
